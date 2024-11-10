68e96cd50cd41315faeb580806ca462f7905ba3f
alvaro <catzs.2022112linf0030@aluno.ifpi.edu.br>
2024-11-10 19:06:37 -0300

    10/11/2024 19:06

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('sobre', views.sobre, name="sobre"),
     path('contato', views.contato, name="contato"),
-    
+    path('item/<int:id>/', views.exibir_item, name='exibir_item'),
+    path('perfil/<str:usuario>/', views.perfil, name='perfil'),
+    path('diadasemana/<int:dia>/', views.diadasemana, name='diadasemana'),
+    path('cadweb/', views.cadweb, name='cadweb'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -12,3 +12,26 @@ def sobre(request):
 def contato(request):
     return render(request,'contato.html')
 
+def exibir_item(request, id):
+    return render(request, "exibir_item.html", {'id':id})
+
+def perfil(request, usuario):
+    return render(request, "perfil.html",{'usuario':usuario})
+
+def diadasemana(request, dia):
+    dias = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado"
+    }
+    if dia in dias:
+        return HttpResponse(f"O dia correspondente é: {dias[dia]}")
+    else:
+        return HttpResponse("Dia inválido")
+    
+def cadweb(request):
+    return render(request,'cadweb.html')
